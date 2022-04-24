21c7a8763012e29965894c16f94a65a5c1b15b64
Gentian Rrafshi <51291479+GentianRrafshi@users.noreply.github.com>
2022-04-25 00:05:32 +0200

    refactor: improve usage and options
    Getops is kinda not what I need here

--- scripts/podcleaner.sh
+++ scripts/podcleaner.sh
@@ -3,23 +3,25 @@
 function usage() {
     cat <<USAGE
 
-    Usage: $0 [-s --status] [-c --context] [-a --all] [-n --namespace] [-f --force]
+    $0 - Delete Pods with specific Status in a Kubernetes Cluster
 
-    Status are:
+    Usage: $0 <options>
+
+    -s | --status <Status>               : Remove Pods with this Status.
+    -c | --context <Context>             : Selects Kubernetes Cluster to operate.
+    -n | --namespace <Namespace>         : Namespace to operate.
+    -a | --all-namespaces                : Operate over all Namespaces.
+    -f | --force                         : Force deletion of Pods.
+    -h | --help                          : Displays help
+
+    Available Status are:
         ContainerCreating
         Error
         ErrImagePull
         Evicted
         ImagePullBackOff
-
-    Options are:
-        -h, --help            Display help for this script.
-        -c, --context         Required. Kuberntes Context to operatore.
-        -s, --status          Required. Wihich Pod Status to delete.
-        -a, --all             Optional. Delete Pods over all Namespaces.
-        -n, --namespace       Optional. Delete Pods over a specific Namespace.
-        -f, --force           Optional. Trigger forcefull deletion.
 USAGE
+
     exit 1
 }
 
@@ -34,56 +36,110 @@ force=
 # over all namespaces
 all_namespaces=false
 
-while getopts "han:s:fc:" option; do
-   case $option in
-      h | -help)      usage ;;
+# Process command line options. See usage above for supported options
+function processOptions() {
+    while [[ $# -gt 0 ]]; do
+        case "$1" in
+           -h | --help)
+              usage
+              exit 0
+           ;;
 
-      c | -context)                CONTEXT="$OPTARG" ;;
+           -c | --context)
+              CONTEXT="$2"
+              if [ -z $2 ]; then
 
-      s | -status)                 STATUS="$OPTARG"  ;;
+                 echo -e "\n     Error: Please provide a valid context to operate on."
+                 usage
+                 exit 1
+              fi
+              shift 2
+           ;;
 
-      a | -all)                    all_namespaces=true ;;
+           -s | --status)
+             STATUS="$2"
+              if [ -z $2 ]; then
 
-      n | -namespace)              NAMESPACE="$OPTARG" ;;
+                 echo -e "\n     Error: Please provide a valid status."
+                 usage
+                 exit 1
+              fi
+             shift 2
+           ;;
 
-      f | -force)                  force=" --force" ;;
+           -a | --all-namespaces)
+              all_namespaces=true
+              shift 1
+           ;;
 
-      \?)            # Invalid option
-                     echo "Error: Invalid option"
-                     usage
-                     ;;
-   esac
-done
+           -n | --namespace)
+              NAMESPACE="$2"
+              if [ -z $2 ]; then
 
-# To make sure deletion happens eiter over all namespace or one specific
-if ([ "$NAMESPACE" ] && [  $all_namespaces == true ])  || ([ -z "$NAMESPACE" ] && [  $all_namespaces == false ]) ; then
-  echo "Either choose operation over all Namespaces (-a) or over a specific Namespace (-n NAMESPACE)!"
+                 echo -e "\n     Error: Please provide a valid namespace to operate on."
+                 usage
+                 exit 1
+              fi
+              shift 2
+           ;;
 
-  exit 1
-fi
+           -f | --force)
+              force=" --force"
+              shift 1
+           ;;
+
+           \?)
+              # Invalid option
+              echo "Error: Invalid option"
+              usage
+            ;;
+
+        esac
+    done
+}
 
 # Main function for deleting pods of certain Status
 function delete() {
 
 if [  $all_namespaces == true  ]; then
-  echo "A total of $(kubectl --context=$CONTEXT get pods -A | grep -c "$STATUS") Pod(s) with Status $STATUS will be deleted ..."
+  echo -e "\n     A total of $(kubectl --context=$CONTEXT get pods -A | grep -c "$STATUS") Pod(s) with Status $STATUS will be deleted ... \n"
 
   kubectl --context=$CONTEXT get pods -A | grep "$STATUS" | awk -v context=$CONTEXT -v force="$force" '{ system ("kubectl --context=" context " delete pod " $2 " -n " $1 force ) }'
 
 elif [ "$NAMESPACE" ] ; then
-  echo "A total of $(kubectl --context=$CONTEXT get pods -n $NAMESPACE | grep -c "$STATUS") Pod(s) with Status $STATUS will be deleted ..."
+  echo -e "\n     A total of $(kubectl --context=$CONTEXT get pods -n $NAMESPACE | grep -c "$STATUS") Pod(s) with Status $STATUS will be deleted ... \n"
 
   kubectl --context=$CONTEXT get pods -n $NAMESPACE | grep $STATUS | awk -v context=$CONTEXT -v force=$force '{ system ("kubectl --context=" context " delete pod " $1 force ) }'
 fi
 
 }
 
+function status() {
 case "$STATUS" in
-  "ContainerCreating"|"ImagePullBackOff"|"Error"|"Evicted"|"ErrImagePull") echo "Removing Pods with the given Status $STATUS"
-                                                       delete
-                                                       ;;
+  "ContainerCreating"|"ImagePullBackOff"|"Error"|"Evicted"|"ErrImagePull")
+     echo -e "\n     Removing Pods with the given Status $STATUS"
+     delete
+  ;;
 
-  *) echo "Given Status in not valid!"
+  *)
+     echo -e "\n     Given Status in not valid!"
      usage
-     ;;
+  ;;
 esac
+}
+
+function main() {
+  processOptions "$@"
+
+  # To make sure deletion happens eiter over all namespace or one specific
+  if ([ "$NAMESPACE" ] && [  $all_namespaces == true ])  || ([ -z "$NAMESPACE" ] && [  $all_namespaces == false ]) ; then
+    echo -e "\n     Error: Either choose operation over all Namespaces (-a) or over a specific Namespace (-n NAMESPACE)!"
+    exit 1
+  fi
+
+  status
+}
+
+######### Main #########
+
+main "$@"
