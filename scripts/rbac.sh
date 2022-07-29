2dc78ea6eefaa32afbcc84308f91c3601e6909bc
Gentian Rrafshi <gentian.rrafshi@de.clara.net>
2022-07-29 13:05:13 +0200

    refactor: add feature who-can and remove redundant code
    - add subcommand who-can to usage with example
    - add who-can subcommand
    - moved empty Namespace handeling in own function
    - restructure output by adding column in end of case not every case and move echo to respective case
    - add eval to handle pipes in variables

--- scripts/rbac.sh
+++ scripts/rbac.sh
@@ -7,13 +7,18 @@ function usage() {
 
     $0 - Gives Informations about (Cluster)Roles and (Cluster)RoleBindings 
 
-    Usage: $0 <options>
+    Usage: $0 <Subcommands> <Options>
 
-    -k | --kind <Kind>                                    Options are Roles, RoleBindings, ClusterRoles and CusterRoleBindings
+    Options:
+    -k | --kind <Kind>                                     Options are Roles, RoleBindings, ClusterRoles and CusterRoleBindings
     -c | --context <Context>                               Selects Kubernetes Cluster to operate.
     -n | --namespace <Namespace1,Namespace2,...>           Namespace to operate. Can be one Namespace or array of Namespaces.
     -h | --help                                            Displays help
 
+    Subcommands:
+    who-can <verb> <Resource>                              Lists all Roles or ClusterRoles that can <verb> <Resource>
+                                                           Example: -k Roles who-can get Pods
+                                                                    Lists all Roles who can get Pods        
 USAGE
     exit 1
 }
@@ -33,8 +38,6 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-NAMESPACE=""
-
 # Process command line options. See usage above for supported options
 function processOptions() {
 
@@ -45,7 +48,18 @@ while [ $# -gt 0 ]; do
   -h | --help)
      usage
   ;;
-    
+
+  who-can) 
+    if ([ -z "$2" ] || [ -z "$3" ]) ; then
+      errorExit "The Option who-can needs a Resource and a Verb!" 
+    fi
+    RESOURCES="| grep -i $3"
+    VERBS="| grep -i $2"
+    shift
+    shift
+    shift
+  ;;
+
   -c | --context)
     kubectl="kubectl --context $2"
     shift
@@ -72,56 +86,49 @@ while [ $# -gt 0 ]; do
 
   \?)
     errorExit "Option not defined or has no argument!"
-    usage
   ;;
   esac
 done 2> /dev/null
 }
 
 
-function rolebindings() {
+function empty_variables() {
 
   if [ -z "$NAMESPACE" ]; then
     NAMESPACE=$(kubectl config view --minify --output 'jsonpath={..namespace}')
-    kubectl get rolebindings -n $NAMESPACE -o custom-columns='NAMESPACE:metadata.namespace,KIND:kind,ROLE:roleRef.name,ROLEKIND:roleRef.kind,NAME:metadata.name,SERVICE-ACCOUNTS:subjects[?(@.kind=="ServiceAccount")].name'
-  else
-    NAMESPACES=$(echo $NAMESPACE | sed "s/,/ /")
-    echo -e "NAMESPACE KIND ROLE ROLEKIND NAME SERVICE-ACCOUNTS"
-    for ns in $NAMESPACES
-    do
-      kubectl get rolebindings -n $ns -o custom-columns='NAMESPACE:metadata.namespace,KIND:kind,ROLE:roleRef.name,ROLEKIND:roleRef.kind,NAME:metadata.name,SERVICE-ACCOUNTS:subjects[?(@.kind=="ServiceAccount")].name'  --no-headers
-    done
   fi
 
 }
 
-function roles() {
+function rolebindings() {
 
-  if [ -z "$NAMESPACE" ] ; then
-    NAMESPACE=$(kubectl config view --minify --output 'jsonpath={..namespace}')
-    echo -e "NAMESPACE KIND NAME RESOURCES \t VERB"
-    kubectl get roles --no-headers -n $NAMESPACE | awk -v NAMESPACE=$NAMESPACE '{ system ("kubectl -n " NAMESPACE " get role " $1 " -o jsonpath='\''{range .rules[*]}{\"\\t\"}{.resources}{\"\\t\"}{.verbs}{\"\\n\"}{end}'\'' \|  sed \"s/^/ "NAMESPACE" Role " $1 "  \/\"  ")  }'
-  else
-    echo -e "NAMESPACE KIND NAME RESOURCES \t VERB"
-    NAMESPACES=$(echo $NAMESPACE | sed "s/,/ /")
-    for ns in $NAMESPACES
-    do 
-      kubectl get roles --no-headers -n $ns | awk -v NAMESPACE=$ns '{ system ("kubectl -n " NAMESPACE " get role " $1 " -o jsonpath='\''{range .rules[*]}{\"\\t\"}{.resources}{\"\\t\"}{.verbs}{\"\\n\"}{end}'\'' \|  sed \"s/^/ "NAMESPACE" Role " $1 "  \/\"  ")  }' 
-    done
-  fi
+  NAMESPACES=$(echo $NAMESPACE | sed "s/,/ /")
+  for ns in $NAMESPACES
+  do
+    kubectl get rolebindings -n $ns -o custom-columns='NAMESPACE:metadata.namespace,KIND:kind,ROLE:roleRef.name,ROLEKIND:roleRef.kind,NAME:metadata.name,SERVICE-ACCOUNTS:subjects[?(@.kind=="ServiceAccount")].name'  --no-headers
+  done
 
 }
 
 function clusterrolebindings() {
 
-  echo -e "NAMESPACE KIND ROLE ROLEKIND NAME SERVICE-ACCOUNTS"
   kubectl get clusterrolebindings -o custom-columns='NAMESPACE:metadata.namespace,KIND:kind,ROLE:roleRef.name,ROLEKIND:roleRef.kind,NAME:metadata.name,SERVICE-ACCOUNTS:subjects[?(@.kind=="ServiceAccount")].name' --no-headers
+
+}
+
+function roles() {
+
+  NAMESPACES=$(echo $NAMESPACE | sed "s/,/ /")
+  for ns in $NAMESPACES
+  do 
+    kubectl get roles --no-headers -n $ns | awk -v NAMESPACE=$ns '{ system ("kubectl -n " NAMESPACE " get role " $1 " -o jsonpath='\''{range .rules[*]}{\"\\t\"}{.resources}{\"\\t\"}{.verbs}{\"\\n\"}{end}'\'' \|  sed \"s/^/ "NAMESPACE" Role " $1 "  \/\"  ")  }' 
+  done
+
 
 }
 
 function clusterroles() {
 
-  echo -e "NAMESPACE KIND NAME RESOURCES \t VERB"
   kubectl get clusterroles --no-headers | awk '{ system ("kubectl get clusterrole " $1 " -o jsonpath='\''{range .rules[*]}{\"\\t\"}{.resources}{\"\\t\"}{.verbs}{\"\\n\"}{end}'\'' \|  sed \"s/^/ <None>  ClusterRole  " $1 " \/\"  ")  }' | column -t
 
 }
@@ -129,31 +136,35 @@ function clusterroles() {
 function main() {
 
   processOptions "$@"
+
+  empty_variables
+
   case "$KIND" in
-    "Roles")
-      roles | column -t
+    "Roles"|"Role")
+      eval "echo -e 'NAMESPACE KIND NAME RESOURCES \t VERB';roles $RESOURCES $VERBS" 
     ;;
-    "ClusterRoles")
-      clusterroles | column -t
+
+    "ClusterRoles"|"ClusterRole")
+      eval "echo -e 'NAMESPACE KIND NAME RESOURCES \t VERB';clusterroles $RESOURCES $VERBS" 
     ;;
-    "RoleBindings")
-       rolebindings
+
+    "RoleBindings"|"RoleBinding")
+       eval "echo -e 'NAMESPACE KIND ROLE ROLEKIND NAME SERVICE-ACCOUNTS';rolebindings" 
     ;;
-    "ClusterRoleBindings")     
-       clusterrolebindings
+
+    "ClusterRoleBindings"|"ClusterRoleBinding")
+      eval "echo -e 'NAMESPACE KIND ROLE ROLEKIND NAME SERVICE-ACCOUNTS';clusterrolebindings" 
     ;;
+
     *)
-      errorExit "Given Kind in not valid!"
+      echo "Given Kind in not valid!"
+      exit 1
     ;;
-    \?)
-      # Invalid option
-      errorExit "Invalid Option" 
-      usage
-    ;;
-  esac
+
+  esac | column -t
 
 }
 
 ######### Main #########
 
-main "$@" 
+main "$@"
